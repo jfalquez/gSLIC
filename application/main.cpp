59de91c31328f6bea9d64ea9e7ea06d83d07a9f9
Juan Falquez <juan.falquez@colorado.edu>
2015-04-16 12:17:11 -0600

    Initial commit.

---
+++ application/main.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2015  Juan M. Falquez,
+ *                     University of Colorado - Boulder
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <Eigen/Eigen>
+#include <sophus/sophus.hpp>
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Woverloaded-virtual"
+#endif
+#include <opencv2/opencv.hpp>
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#endif
+#include <calibu/Calibu.h>
+#include <HAL/Utils/GetPot>
+#include <HAL/Camera/CameraDevice.h>
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+#include <pangolin/pangolin.h>
+#include <SceneGraph/SceneGraph.h>
+
+#include <gSLIC/FastImgSeg.h>
+
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+int main(int argc, char** argv)
+{
+
+  return 0;
+}
